2ad802f52c0b3b3e511774cd5d5e79e9286b7a1a
Tim S <racer4308@gmail.com>
2023-11-15 13:48:59 +0100

    WIP

--- app/frontend/angular-frontend/src/app/navbar/navbar.component.ts
+++ app/frontend/angular-frontend/src/app/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {NgbInputDatepicker, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, NgbInputDatepicker],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   faHouse = faHouse;
   faUser = faUser;
+
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
