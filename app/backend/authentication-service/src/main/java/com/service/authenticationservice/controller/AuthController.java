f69abca8b099b6d74df0deee9f6e786a8763c8ff
Eike Torben Menzel <eike.torben.menzel@mni.thm.de>
2023-11-26 06:23:12 +0100

    ADDED Statuscodes for MessageResponseDTO

--- app/backend/authentication-service/src/main/java/com/service/authenticationservice/controller/AuthController.java
+++ app/backend/authentication-service/src/main/java/com/service/authenticationservice/controller/AuthController.java
@@ -52,22 +52,22 @@ public class AuthController {
     public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequest) {
 
         if (registerRequest.username().length() < 4 || registerRequest.username().length() > 30) {
-            return ResponseEntity.badRequest().body(new MessageResponseDTO("Error, Username is to long or to short"));
+            return ResponseEntity.badRequest().body(new MessageResponseDTO(2, "Error, Username is to long or to short"));
         }
 
         if (passwordSecurityService.checkPasswordIsRainbow(registerRequest.password())) //contains check, returns true if the password is in the table
-            return ResponseEntity.badRequest().body(new MessageResponseDTO("Error, Password is a blocked password"));
+            return ResponseEntity.badRequest().body(new MessageResponseDTO(3, "Error, Password is a blocked password"));
 
         if (!passwordSecurityService.checkPasswordSecurity(registerRequest.password())) { //false if requirements not meet
-            return ResponseEntity.badRequest().body(new MessageResponseDTO("Error, Please make sure you are using at least 1x digit, 1x capitalized and 1x lower-case letter and at least 1x symbol from the following pool: ~`! @#$%^&*()_-+={[}]|:;<,>.?/"));
+            return ResponseEntity.badRequest().body(new MessageResponseDTO(3, "Error, Please make sure you are using at least 1x digit, 1x capitalized and 1x lower-case letter and at least 1x symbol from the following pool: ~`! @#$%^&*()_-+={[}]|:;<,>.?/"));
         }
 
         if (!EmailValidator.validate(registerRequest.email())) { //checks for Regex of an email + email-length
-            return ResponseEntity.badRequest().body(new MessageResponseDTO("Error, invalid Email"));
+            return ResponseEntity.badRequest().body(new MessageResponseDTO(1, "Error, invalid Email"));
         }
 
         if (dbQueryService.doesUserWithEmailExist(registerRequest.email()).isPresent()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new MessageResponseDTO("Error, Email already exists"));
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new MessageResponseDTO(1, "Error, Email already exists"));
         }
 
         HttpStatusCode resultSaveUser = dbQueryService.saveUser(new UserDTO(registerRequest.username(), registerRequest.email(), encoder.encode(registerRequest.password()), skipVerify));
@@ -79,12 +79,12 @@ public class AuthController {
             Optional<Long> userId = dbQueryService.getUserIdByEmail(registerRequest.email());
             if (userId.isEmpty()) //Information could not be fetched from database
             {
-                return ResponseEntity.internalServerError().body(new MessageResponseDTO("Error, some problem occurred"));
+                return ResponseEntity.internalServerError().body(new MessageResponseDTO(-1, "Error, some problem occurred"));
             }
 
             emailQueryService.sendEmail(userId.get(), MailType.VERIFICATION);
         } else {
-            return ResponseEntity.internalServerError().body(new MessageResponseDTO("Error, some problem occurred"));
+            return ResponseEntity.internalServerError().body(new MessageResponseDTO(-1, "Error, some problem occurred"));
         }
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -101,7 +101,7 @@ public class AuthController {
             UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
             if (dbQueryService.getVerificationStateUser(userDetails.id()).isEmpty())
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponseDTO("Authentication failed: The user is not verified"));
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponseDTO(4, "Authentication failed: The user is not verified"));
 
             ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
 
@@ -111,7 +111,7 @@ public class AuthController {
         } catch (AuthenticationException e) {
             logger.error("Authentication failed: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new MessageResponseDTO("Authentication failed: " + e.getMessage()));
+                    .body(new MessageResponseDTO(-1, "Authentication failed: " + e.getMessage()));
         }
     }
 
@@ -121,11 +121,11 @@ public class AuthController {
         if (httpStatusCode.equals(HttpStatus.NO_CONTENT)) {
             return ResponseEntity.noContent().build();
         } else if (httpStatusCode.equals(HttpStatus.CONFLICT)) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new MessageResponseDTO("Email is already verified"));
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new MessageResponseDTO(1, "Email is already verified"));
         } else if (httpStatusCode.equals(HttpStatus.BAD_REQUEST)) {
-            return ResponseEntity.badRequest().body(new MessageResponseDTO("Token is not acceptable"));
+            return ResponseEntity.badRequest().body(new MessageResponseDTO(5, "Token is not acceptable"));
         } else if (httpStatusCode.is5xxServerError()) {
-            return ResponseEntity.internalServerError().body(new MessageResponseDTO("An error has occurred, please try again later"));
+            return ResponseEntity.internalServerError().body(new MessageResponseDTO(-1, "An error has occurred, please try again later"));
         }
         return ResponseEntity.unprocessableEntity().build();
     }
