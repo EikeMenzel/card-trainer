2d54023076fb44b2a55073fa4c5cfbb191950e2a
Eike Torben Menzel <eike.torben.menzel@mni.thm.de>
2023-12-01 00:19:17 +0100

    ADDED Feature "GET detailed history-information"

--- app/backend/database-service/src/main/java/com/service/databaseservice/repository/sessions/LearnSessionRepository.java
+++ app/backend/database-service/src/main/java/com/service/databaseservice/repository/sessions/LearnSessionRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface LearnSessionRepository extends JpaRepository<LearnSession, Long> {
     Optional<LearnSession> findTopByDeckIdAndUserIdOrderByIdDesc(Long deckId, Long userId);
     List<LearnSession> getAllByUserId(Long userId);
+    Optional<LearnSession> getLearnSessionByIdAndUserId(Long sessionId, Long userId);
 }
