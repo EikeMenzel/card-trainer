b8e9cec5bc9ce0d19fbbed2f7cfcb31f388be789
Maria Kazim <maria.kazim@mni.thm.de>
2023-12-13 01:38:25 +0100

    ADDED User Profile Screen to Frontend

--- app/frontend/angular-frontend/src/app/services/user-service/user.service.ts
+++ app/frontend/angular-frontend/src/app/services/user-service/user.service.ts
@@ -1,13 +1,31 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpStatusCode} from "@angular/common/http";
 import {MessageResponseDTO} from "../../models/MessageResponseDTO";
+import {UserInfoDTO} from "../../models/UserInfoDTO";
+import {AuthService} from "../auth-service/auth-service";
+import {Router} from "@angular/router";
+import {ToastService} from "../toast-service/toast.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) {
+  private userData: UserInfoDTO = {
+    username: "",
+    email: "",
+    achievementIds: [],
+    langCode: "EN",
+    cardsToLearn: 0,
+    receiveLearnNotification: false
+  };
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router,
+    private toast: ToastService
+  ) {
   }
 
   sendResetPasswordRequest(email: string) {
@@ -16,5 +34,39 @@ export class UserService {
 
   requestPasswordReset(password: string, token: string, email: string) {
     return this.http.put("/api/v1/password/reset", {password, token, email}, {observe: "response"})
+  }
+
+  changePassword(newPassword: string) {
+    return this.http.put('api/v1/password', {"password": newPassword}, {observe: "response"})
+  }
+
+  updateUserInfo(userProfile: UserInfoDTO) {
+    return this.http.put<UserInfoDTO>('api/v1/account', userProfile)
+  }
+
+  getUserInfoDTO(): UserInfoDTO {
+    if (this.userData.username == "") {
+      this.getUpdatedUserInfo()
+    }
+
+    return this.userData
+  }
+
+  getUpdatedUserInfo() {
+    const request: XMLHttpRequest = new XMLHttpRequest();
+    request.open('GET', "/api/v1/account", false);
+    request.send(null);
+    if (request.status == HttpStatusCode.Ok) {
+      this.userData = JSON.parse(request.responseText);
+      return
+    }
+
+    if (request.status == HttpStatusCode.PreconditionFailed) {
+      this.auth.logout()
+      this.router.navigate(["/login"])
+    }
+
+    if(request.status != HttpStatusCode.Unauthorized)
+      this.toast.showErrorToast("User Loading error", "Could not load user Data")
   }
 }
