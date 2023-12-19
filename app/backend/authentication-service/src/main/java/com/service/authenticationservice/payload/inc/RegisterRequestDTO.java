f682d30f6fe7fb0f4932f1257e547af781927f06
Eike <eike.torben.menzel@mni.thm.de>
2023-12-19 02:18:16 +0100

    ADDED Input Validations

--- app/backend/authentication-service/src/main/java/com/service/authenticationservice/payload/inc/RegisterRequestDTO.java
+++ app/backend/authentication-service/src/main/java/com/service/authenticationservice/payload/inc/RegisterRequestDTO.java
@@ -2,6 +2,7 @@ package com.service.authenticationservice.payload.inc;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record RegisterRequestDTO(@NotBlank String username, @Email @NotBlank String email, @NotBlank String password) {
+public record RegisterRequestDTO(@NotBlank @Size(min = 4, max = 30) String username, @Email @NotBlank @Size(min = 6, max = 64) String email, @NotBlank @Size(min = 8, max = 64) String password) {
 }
