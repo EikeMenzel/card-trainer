20fdc288ebfdf211bf028d401567f96a35fd63ba
Eike Torben Menzel <eike.torben.menzel@mni.thm.de>
2023-12-27 00:04:30 +0100

    Resolve "BUG: Error-Messages if validation fails is not properly implemented - auth.service"

--- app/backend/authentication-service/src/main/java/com/service/authenticationservice/payload/inc/MailDTO.java
+++ app/backend/authentication-service/src/main/java/com/service/authenticationservice/payload/inc/MailDTO.java
@@ -2,5 +2,5 @@ package com.service.authenticationservice.payload.inc;
 
 import jakarta.validation.constraints.Size;
 
-public record MailDTO(@Size(min = 6, max = 64) String email) {
+public record MailDTO(@Size(min = 6, max = 64, message = "E-Mail is not valid!") String email) {
 }
