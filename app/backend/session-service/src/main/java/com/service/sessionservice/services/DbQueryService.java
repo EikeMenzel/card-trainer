1a1c755e0e4e9c8f5f71ae379308e5ee4eb4ec35
Eike Torben Menzel <eike.torben.menzel@mni.thm.de>
2023-12-31 17:31:00 +0100

    ADDED Peek-Session Functionality

--- app/backend/session-service/src/main/java/com/service/sessionservice/services/DbQueryService.java
+++ app/backend/session-service/src/main/java/com/service/sessionservice/services/DbQueryService.java
@@ -89,4 +89,64 @@ public class DbQueryService {
             return Pair.of(e.getStatusCode(), Optional.empty());
         }
     }
+
+    public Pair<HttpStatusCode, Long> savePeekSession(Long userId, Long deckId) {
+        try {
+            var headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(
+                    DB_API_BASE_PATH + "/users/" + userId + "/decks/" + deckId + "/peek-sessions",
+                    new HttpEntity<>(headers),
+                    String.class);
+            return Pair.of(responseEntity.getStatusCode(), Long.valueOf(Objects.requireNonNull(responseEntity.getBody())));
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            logger.debug(e.getMessage());
+            return Pair.of(e.getStatusCode(), null);
+        }
+    }
+
+    public HttpStatusCode savePeekSessionCard(Long userId, Long peekSessionId, Long cardId) {
+        try {
+            var headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(
+                    DB_API_BASE_PATH + "/users/" + userId + "/peek-sessions/" + peekSessionId +  "/cards/" + cardId,
+                    new HttpEntity<>(headers),
+                    String.class);
+            return responseEntity.getStatusCode();
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            logger.debug(e.getMessage());
+            return e.getStatusCode();
+        }
+    }
+
+    public HttpStatusCode updatePeekSessionStatus(Long userId, Long peekSessionId, StatusTypeDTO statusTypeDTO) {
+        try {
+            var headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            ResponseEntity<Void> responseEntity = restTemplate.exchange(
+                    DB_API_BASE_PATH + "/users/" + userId + "/peek-sessions/" + peekSessionId + "/status",
+                    HttpMethod.PUT,
+                    new HttpEntity<>(statusTypeDTO, headers),
+                    Void.class
+            );
+            return responseEntity.getStatusCode();
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            logger.debug(e.getMessage());
+            return e.getStatusCode();
+        }
+    }
+
+    public Pair<HttpStatusCode, Object> getRandomPeekSessionCard(Long userId, Long peekSessionId) {
+        try {
+            String url = DB_API_BASE_PATH + "/users/" + userId + "/peek-sessions/" + peekSessionId + "/cards/random-card";
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+            return (responseEntity.getStatusCode() == HttpStatus.OK)
+                    ? Pair.of(HttpStatus.OK, Optional.ofNullable(responseEntity.getBody()))
+                    : Pair.of(responseEntity.getStatusCode(), Optional.empty());
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            logger.debug(e.getMessage());
+            return Pair.of(e.getStatusCode(), Optional.empty());
+        }
+    }
 }
