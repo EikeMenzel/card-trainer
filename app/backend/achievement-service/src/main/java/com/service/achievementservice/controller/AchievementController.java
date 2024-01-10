781a5c43e8bfadcf41958029cf02b8f8939384fb
Eike Torben Menzel <eike.torben.menzel@mni.thm.de>
2024-01-10 13:41:40 +0100

    ADDED "API-Documentation AchievementService"

--- app/backend/achievement-service/src/main/java/com/service/achievementservice/controller/AchievementController.java
+++ app/backend/achievement-service/src/main/java/com/service/achievementservice/controller/AchievementController.java
@@ -3,6 +3,11 @@ package com.service.achievementservice.controller;
 import com.service.achievementservice.payload.AchievementDetailsDTO;
 import com.service.achievementservice.services.AchievementService;
 import com.service.achievementservice.services.DbQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,30 +22,78 @@ public class AchievementController {
         this.dbQueryService = dbQueryService;
     }
 
-    @PostMapping("/users/{userId}/achievements/check-deck")
-    public void checkDeckAchievements(@PathVariable Long userId) {
+    @PostMapping("/{userId}/achievements/check-deck")
+    @Operation(summary = "Check Deck Achievements",
+            description = "Triggers the check for deck-related achievements for the specified user.<br><br>" +
+                    "<strong>Note:</strong> User ID must be valid." +
+                    "<strong>⚠️ Warning:</strong> This method might not work correctly in Swagger UI due to internal routes which are not exposed to the frontend.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Deck achievements check initiated"),
+                    @ApiResponse(responseCode = "400", description = "Bad request"),
+                    @ApiResponse(responseCode = "500", description = "Server could not be reached")
+            })
+    public void checkDeckAchievements(@Parameter(description = "User ID for checking achievements", required = true) @PathVariable Long userId) {
         achievementService.achievementCreationDeck(userId);
     }
 
-    @PostMapping("/users/{userId}/achievements/check-session")
-    public void checkSessionAchievements(@PathVariable Long userId) {
+    @PostMapping("/{userId}/achievements/check-session")
+    @Operation(summary = "Check Session Achievements",
+            description = "Triggers the check for session-related achievements for the specified user.<br><br>" +
+                    "<strong>Note:</strong> User ID must be valid." +
+                    "<strong>⚠️ Warning:</strong> This method might not work correctly in Swagger UI due to internal routes which are not exposed to the frontend.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Deck achievements check initiated"),
+                    @ApiResponse(responseCode = "400", description = "Bad request"),
+                    @ApiResponse(responseCode = "500", description = "Server could not be reached")
+            })
+    public void checkSessionAchievements(@Parameter(description = "User ID for checking achievements", required = true) @PathVariable Long userId) {
         achievementService.achievementSessionDaily(userId);
         achievementService.achievementStateSessions(userId);
     }
 
-    @PostMapping("/users/{userId}/achievements/check-cards-learned")
-    public void checkCardsLearnedAchievements(@PathVariable Long userId) {
+    @PostMapping("/{userId}/achievements/check-cards-learned")
+    @Operation(summary = "Check Cards Learned Achievements",
+            description = "Triggers the check for cards learned achievements for the specified user.<br><br>" +
+                    "<strong>Note:</strong> User ID must be valid." +
+                    "<strong>⚠️ Warning:</strong> This method might not work correctly in Swagger UI due to internal routes which are not exposed to the frontend.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Deck achievements check initiated"),
+                    @ApiResponse(responseCode = "400", description = "Bad request"),
+                    @ApiResponse(responseCode = "500", description = "Server could not be reached")
+            })
+    public void checkCardsLearnedAchievements(@Parameter(description = "User ID for checking achievements", required = true) @PathVariable Long userId) {
         achievementService.achievementCardsLearnedDaily(userId);
         achievementService.achievementCardsLearned(userId);
     }
 
-    @PostMapping("/users/{userId}/achievements/check-daily-login")
-    public void checkDailyLoginAchievement(@PathVariable Long userId) {
+    @PostMapping("/{userId}/achievements/check-daily-login")
+    @Operation(summary = "Check Daily Login Achievement",
+            description = "Triggers the check for the daily login achievement for the specified user.<br><br>" +
+                    "<strong>Note:</strong> User ID must be valid." +
+                    "<strong>⚠️ Warning:</strong> This method might not work correctly in Swagger UI due to internal routes which are not exposed to the frontend.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Deck achievements check initiated"),
+                    @ApiResponse(responseCode = "400", description = "Bad request"),
+                    @ApiResponse(responseCode = "500", description = "Server could not be reached")
+            })
+    public void checkDailyLoginAchievement(@Parameter(description = "User ID for checking achievements", required = true) @PathVariable Long userId) {
         achievementService.achievementDailyLogin(userId);
     }
 
     @GetMapping("/achievements/{achievementId}")
-    public ResponseEntity<AchievementDetailsDTO> getAchievementDetails(@RequestHeader Long userId, @PathVariable Long achievementId) {
+    @Operation(summary = "Retrieve Achievement Details",
+            description = "Fetches the details of a specific achievement by its ID.<br><br>" +
+                    "<strong>Note:</strong> The achievement ID must be valid. User ID is required for authorization purposes." +
+                    "<br><br><strong>⚠️ Warning:</strong> This route will not work, if you aren't logged in!",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Achievement details successfully retrieved",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AchievementDetailsDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Achievement not found"),
+                    @ApiResponse(responseCode = "500", description = "Server could not be reached")
+            })
+    public ResponseEntity<AchievementDetailsDTO> getAchievementDetails(
+            @Parameter(description = "User ID of the requester", required = true) @RequestHeader Long userId,
+            @Parameter(description = "ID of the achievement to retrieve details for", required = true) @PathVariable Long achievementId) {
         return dbQueryService.getAchievementDetails(achievementId)
                 .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
