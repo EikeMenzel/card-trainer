bcc41970bf5c04d5ac1b1a8e911bd68567dd7d8d
Eike <eike.torben.menzel@mni.thm.de>
2024-01-11 12:20:59 -0700

    FIXED Achievement Path error

--- app/backend/cards-service/src/main/java/com/service/cardsservice/services/AchievementQueryService.java
+++ app/backend/cards-service/src/main/java/com/service/cardsservice/services/AchievementQueryService.java
@@ -1,12 +1,17 @@
 package com.service.cardsservice.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AchievementQueryService {
+    private final Logger logger = LoggerFactory.getLogger(AchievementQueryService.class);
     private final RestTemplate restTemplate;
     private final String achievementServiceUrl;
     public AchievementQueryService(RestTemplate restTemplate, @Value("${achievement-service.api.path}") String achievementServiceUrl) {
@@ -14,23 +19,29 @@ public class AchievementQueryService {
         this.achievementServiceUrl = achievementServiceUrl + "/api/v1";
     }
 
-    public HttpStatusCode checkDeckAchievements(Long userId) {
-        var headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(
-                achievementServiceUrl + "/users/" + userId + "/achievements/check-deck",
-                new HttpEntity<>(headers),
-                String.class);
-        return responseEntity.getStatusCode();
+    public void checkDeckAchievements(Long userId) {
+        try {
+            var headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            restTemplate.postForEntity(
+                    achievementServiceUrl + "/users/" + userId + "/achievements/check-deck",
+                    new HttpEntity<>(headers),
+                    String.class);
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            logger.debug(e.getMessage());
+        }
     }
 
-    public HttpStatusCode checkDailyLoginAchievement(Long userId) {
-        var headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(
-                achievementServiceUrl + "/users/" + userId + "/achievements/check-daily-login",
-                new HttpEntity<>(headers),
-                String.class);
-        return responseEntity.getStatusCode();
+    public void checkDailyLoginAchievement(Long userId) {
+        try {
+            var headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            restTemplate.postForEntity(
+                    achievementServiceUrl + "/users/" + userId + "/achievements/check-daily-login",
+                    new HttpEntity<>(headers),
+                    String.class);
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            logger.debug(e.getMessage());
+        }
     }
 }
