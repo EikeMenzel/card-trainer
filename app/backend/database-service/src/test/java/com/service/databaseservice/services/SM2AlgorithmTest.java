495475043908218cc9c7e0e4ae32ce49712deaa7
Eike <eike.torben.menzel@mni.thm.de>
2024-01-16 00:34:21 +0100

    BUGFIX Pipeline

--- app/backend/database-service/src/test/java/com/service/databaseservice/services/SM2AlgorithmTest.java
+++ app/backend/database-service/src/test/java/com/service/databaseservice/services/SM2AlgorithmTest.java
@@ -1,6 +1,7 @@
 package com.service.databaseservice.services;
 
-import com.service.databaseservice.model.Repetition;
+import com.service.databaseservice.model.RepetitionModel;
+import com.service.databaseservice.model.RepetitionModel;
 import com.service.databaseservice.payload.inc.learnsession.RatingLevelDTO;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SM2AlgorithmTest {
     @Test
     void testCalc_FirstRepetition() {
-        Repetition repetition = new Repetition();
+        RepetitionModel repetition = new RepetitionModel();
         repetition.setRepetition(0);
         repetition.setPrevEaseFactor(2.5);
         repetition.setPrevInterval(0);
@@ -17,7 +18,7 @@ class SM2AlgorithmTest {
 
         RatingLevelDTO ratingLevelDTO = RatingLevelDTO.RATING_5;
 
-        Repetition updatedRepetition = SM2Algorithm.calc(repetition, ratingLevelDTO);
+        RepetitionModel updatedRepetition = SM2Algorithm.calc(repetition, ratingLevelDTO);
 
         assertEquals(1, updatedRepetition.getRepetition());
         assertEquals(1, updatedRepetition.getPrevInterval());
@@ -25,7 +26,7 @@ class SM2AlgorithmTest {
 
     @Test
     void testCalc_SecondRepetition() {
-        Repetition repetition = new Repetition();
+        RepetitionModel repetition = new RepetitionModel();
         repetition.setRepetition(1);
         repetition.setPrevEaseFactor(2.5);
         repetition.setPrevInterval(1);
@@ -33,7 +34,7 @@ class SM2AlgorithmTest {
         RatingLevelDTO ratingLevelDTO = RatingLevelDTO.RATING_3;
 
         // Execute
-        Repetition updatedRepetition = SM2Algorithm.calc(repetition, ratingLevelDTO);
+        RepetitionModel updatedRepetition = SM2Algorithm.calc(repetition, ratingLevelDTO);
 
         // Verify
         assertEquals(2, updatedRepetition.getRepetition());
@@ -42,14 +43,14 @@ class SM2AlgorithmTest {
 
     @Test
     void testCalc_SubsequentRepetitions() {
-        Repetition repetition = new Repetition();
+        RepetitionModel repetition = new RepetitionModel();
         repetition.setRepetition(2);
         repetition.setPrevEaseFactor(2.5);
         repetition.setPrevInterval(6);
 
         RatingLevelDTO ratingLevelDTO = RatingLevelDTO.RATING_3;
 
-        Repetition updatedRepetition = SM2Algorithm.calc(repetition, ratingLevelDTO);
+        RepetitionModel updatedRepetition = SM2Algorithm.calc(repetition, ratingLevelDTO);
 
         assertEquals(3, updatedRepetition.getRepetition());
         assertEquals(2.36, updatedRepetition.getPrevEaseFactor());
