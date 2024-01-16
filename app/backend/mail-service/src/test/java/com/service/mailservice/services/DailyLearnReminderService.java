a43b3e7a8f71a1136f525b467478620d03d62158
Eike <eike.torben.menzel@mni.thm.de>
2024-01-17 00:36:26 +0100

    ADDED Tests Mail-Service

---
+++ app/backend/mail-service/src/test/java/com/service/mailservice/services/DailyLearnReminderService.java
@@ -0,0 +1,42 @@
+package com.service.mailservice.services;
+
+import com.service.mailservice.payload.inc.UserDailyReminderDTO;
+import com.service.mailservice.services.DbQueryService;
+import com.service.mailservice.services.MailService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+class DailyLearnReminderServiceTest {
+
+    @Mock
+    private DbQueryService dbQueryService;
+
+    @Mock
+    private MailService mailService;
+
+    @Test
+    void testDailyLearnEmail() {
+        List<UserDailyReminderDTO> userReminderList = Arrays.asList(
+                new UserDailyReminderDTO("user1", "user1@example.com"),
+                new UserDailyReminderDTO("user2", "user2@example.com")
+        );
+
+        Mockito.when(dbQueryService.getAllEmailsForDailyLearn()).thenReturn(Optional.of(userReminderList));
+
+        DailyLearnReminderService reminderService = new DailyLearnReminderService(dbQueryService, mailService);
+        ReflectionTestUtils.setField(reminderService, "dbQueryService", dbQueryService);
+        ReflectionTestUtils.setField(reminderService, "mailService", mailService);
+
+        reminderService.dailyLearnEmail();
+
+        Mockito.verify(mailService, Mockito.times(2)).sendDailyLearnReminderMail(Mockito.anyString(), Mockito.anyString());
+    }
+}
