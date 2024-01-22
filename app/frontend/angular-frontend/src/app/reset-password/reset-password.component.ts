bb8ebc2fdcea9cce7dc2fefeaa913af91dcf6e67
Maria Kazim <maria.kazim@mni.thm.de>
2024-01-22 04:57:13 +0100

    FIXED error handling reset password screen

--- app/frontend/angular-frontend/src/app/reset-password/reset-password.component.ts
+++ app/frontend/angular-frontend/src/app/reset-password/reset-password.component.ts
@@ -1,11 +1,14 @@
 import {Component} from '@angular/core';
 import {Router, ActivatedRoute, RouterLink} from '@angular/router';
-import {NgIf} from "@angular/common";
+import {NgIf, NgOptimizedImage} from "@angular/common";
 import {UserService} from "../services/user-service/user.service";
 import {ToastService} from "../services/toast-service/toast.service";
 import {CommonModule} from '@angular/common';
 import {FormsModule} from "@angular/forms";
-import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
+import {HttpStatusCode} from "@angular/common/http";
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 
 
 @Component({
@@ -17,69 +20,102 @@ import {TranslateModule, TranslateService} from "@ngx-translate/core";
     RouterLink,
     CommonModule,
     FormsModule,
+    FaIconComponent,
+    NgOptimizedImage,
     TranslateModule
   ],
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
   isLoading: boolean = false;
+  protected readonly faEye = faEye;
+  protected readonly faEyeSlash = faEyeSlash;
+  showPassword: boolean = false;
+  showPasswordRepeat: boolean = false;
+  isButtonPressed: boolean = false;
+  passwordError: string = "";
+  passwordRepeatError: string = "";
+  newPassword: string = '';
+  reenterNewPassword: string = '';
+  private statusCode: HttpStatusCode | undefined;
+
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private userService: UserService,
     private toast: ToastService,
-    private translate: TranslateService
-  ) {}
-
-  comparePassword(firstPw: string, secondPw: string) {
-    return firstPw !== secondPw && secondPw !== ''
+    private toastService: ToastService,
+    private translate: TranslateService) {
   }
 
   submit(newPw: string) {
-    if (newPw.length < 8) {
-      this.toast.showErrorToast(this.translate.instant("password_error"), this.translate.instant("password_min_length"))
-      return
+
+    this.passwordError = "";
+    this.passwordRepeatError = "";
+
+    // Check if newPassword and reenterNewPassword are empty
+    if (!this.newPassword || !this.reenterNewPassword) {
+      this.passwordError = this.translate.instant("field_cannot_be_empty");
+      this.passwordRepeatError = this.translate.instant("field_cannot_be_empty");
+      return;
     }
 
-    if (newPw.length > 72) {
-      this.toast.showErrorToast(this.translate.instant("password_error"), this.translate.instant("password_max_length"))
-      return
+    // Check if newPassword and reenterNewPassword match
+    if (this.newPassword !== this.reenterNewPassword) {
+      this.passwordError = this.translate.instant("passwords_do_not_match");
+      this.passwordRepeatError = this.translate.instant("passwords_do_not_match");
+      return;
     }
 
-    this.isLoading = true
-    this.route.queryParams.subscribe(value => {
-      const token = value["token"]
-      const email = value["email"]
+    // Check if newPassword length is between 8 and 72 characters
+    if (this.newPassword.length < 8) {
+      this.passwordError = this.translate.instant("password_min_length");
+      return;
+    } else if (this.newPassword.length > 72) {
+      this.passwordError = this.translate.instant("password_max_length");
+      return;
+    }
+
+    this.isButtonPressed = true;
+    this.isLoading = true;
+    this.route.queryParams.subscribe((value) => {
+      const token = value['token'];
+      const email = value['email'];
       this.userService.requestPasswordReset(newPw, token, email).subscribe({
         next: res => {
           this.toast.showSuccessToast(this.translate.instant("password_reset"), this.translate.instant("password_reset_success"))
           this.router.navigate(["/login"]);
+          this.isButtonPressed = false;
           this.isLoading = false
         },
         error: err => {
-          console.log(err)
           this.isLoading = false
           const errStatus = err.error.status as number
           switch (errStatus) {
             case 3: {
-              this.toast.showErrorToast(this.translate.instant("password_reset"), this.translate.instant("password_constraint"))
+              this.passwordError = this.translate.instant("password_constraint");
               break
             }
             case 4: {
-              this.toast.showErrorToast(this.translate.instant("password_reset"), this.translate.instant("verification_constraint"))
+              this.passwordError = this.translate.instant("verification_constraint");
               break
             }
             case 5: {
-              this.toast.showErrorToast(this.translate.instant("password_reset"), this.translate.instant("reset_token_response"))
+              this.passwordError = this.translate.instant("reset_token_response");
               break
             }
             default: {
-              this.toast.showErrorToast(this.translate.instant("password_reset"), this.translate.instant("password_reset_default"))
+              this.passwordError = this.translate.instant("password_reset_default");
             }
           }
-        }
-      })
-    })
+          if (this.statusCode == HttpStatusCode.InternalServerError) {
+            this.toast.showErrorToast(this.translate.instant("password_reset"), this.translate.instant("server_unreachable"))
+          }
+          this.isButtonPressed = false;
+          return;
+        },
+      });
+    });
   }
 }
